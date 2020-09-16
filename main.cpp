51572c789adb0df456888265b8346fb3def25a13
adaev-git <adaev@icm.krasn.ru>
2020-09-16 20:21:05 +0700

    staert

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int a = 0;
+    a ++;
+
+    a += 10;
+    a += 99;
+
+    std :: cout << a << std :: endl;
+    return 0;
+}
